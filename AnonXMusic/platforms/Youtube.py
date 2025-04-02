83a30ad52c6b7d4062a184acee66fc249a601894
KALI-OFFICIAL1 <kaliofficial448@gmail.com>
2025-04-02 11:17:41 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -11,8 +11,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AarohiX.utils.database import is_on_off
-from AarohiX.utils.formatters import time_to_seconds
+from AnonXMusic.utils.database import is_on_off
+from AnonXMusic.utils.formatters import time_to_seconds
 
 
 def cookie_txt_file():
